ab6ac1279a8b74b5403bdbe778b9b57004b794cb
jhiy2004 <jhiy2004@gmail.com>
2024-09-10 14:15:46 -0300

    Adicionando funções para a classe histórico médico

--- src/modelo/HistoricoMedico.java
+++ src/modelo/HistoricoMedico.java
@@ -1,9 +1,9 @@
 package modelo;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class HistoricoMedico{
-	private Date dataCriacao;
+	private LocalDateTime dataModificacao;
 	private String historicoFamiliar;
 	private String historicoMedicamentos;
 	private String historicoCirurgias;
@@ -12,11 +12,59 @@ public class HistoricoMedico{
 
 	public HistoricoMedico(String historicoFamiliar, String historicoMedicamentos, String historicoCirurgias,
 							String doencasPrevias, String doencasCronicas){
-		dataCriacao = new Date();
+		this.dataModificacao = LocalDateTime.now();
 		this.historicoFamiliar = historicoFamiliar;
 		this.historicoMedicamentos = historicoMedicamentos;
 		this.historicoCirurgias = historicoCirurgias;
 		this.doencasPrevias = doencasPrevias;
 		this.doencasCronicas = doencasPrevias;
+	}
+
+	public LocalDateTime getData(){
+		return this.dataModificacao;
+	}
+
+	public String getHistoricoFamiliar(){
+		return this.historicoFamiliar;
+	}
+
+	public String getHistoricoMedicamentos(){
+		return this.historicoMedicamentos;
+	}
+
+	public String getHistoricoCirurgias(){
+		return this.historicoCirurgias;
+	}
+
+	public String getDoencasPrevias(){
+		return this.doencasPrevias;
+	}
+
+	public String getDoencasCronicas(){
+		return this.doencasCronicas;
+	}
+
+	public void setHistoricoMedicamentos(String historicoMedicamentos){
+		this.historicoMedicamentos = historicoMedicamentos;
+		this.atualizarDataParaAtual();
+	}
+
+	public void setHistoricoCirurgias(String historicoCirurgias){
+		this.historicoCirurgias = historicoCirurgias;
+		this.atualizarDataParaAtual();
+	}
+
+	public void setDoencasPrevias(String doencasPrevias){
+		this.doencasPrevias = doencasPrevias;
+		this.atualizarDataParaAtual();
+	}
+	
+	public void setDoencasCronicas(String doencasCronicas){
+		this.doencasCronicas = doencasCronicas;
+		this.atualizarDataParaAtual();
+	}
+
+	public void atualizarDataParaAtual(){
+		this.dataModificacao = LocalDateTime.now();
 	}
 }
