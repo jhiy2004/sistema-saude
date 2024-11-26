404da8e1ac8256b7c6f3f847658a4e1cb6f9eb55
DaniPadua <daniel.aa.padua@unesp.br>
2024-11-26 04:43:42 -0300

    Documentação interface

--- src/interfaces/Relatorio.java
+++ src/interfaces/Relatorio.java
@@ -1,8 +1,23 @@
 package interfaces;
 
 import modelo.Hospital;
-
+/**
+ * Interface que define os métodos necessários para gerar e exibir relatórios relacionados ao hospital.
+ * As classes que implementam essa interface são responsáveis por fornecer a lógica específica
+ * para gerar relatórios diferentes(departamentos, medicamentos)
+ */
 public interface Relatorio {
+     /**
+     * Gera um relatório com base nas informações do hospital.
+     *
+     * @param h O hospital cujas informações serão incluídas no relatório.
+     * @return Uma string contendo o relatório formatado.
+     */
     String gerarRelatorio(Hospital h);
+    /**
+     * Exibe o relatório.
+     *
+     * @param h O hospital cujas informações serão incluídas no relatório.
+     */
     void exibirRelatorio(Hospital h);
 }
